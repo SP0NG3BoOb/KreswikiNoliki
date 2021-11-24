7bdc2ec9b06c6d196c2a1bdd39d2b3cc3e7917db
SP0NG3 <aleksandre.kiknadze.1@btu.edu.ge>
2021-11-24 15:55:52 +0400

    good

---
+++ app/src/main/java/com/example/kreswikinoliki/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.kreswikinoliki
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var Player1 : EditText
+    private lateinit var Player2 : EditText
+    private lateinit var startButton : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Player1 = findViewById(R.id.Player1)
+        Player2 = findViewById(R.id.Player2)
+        startButton = findViewById(R.id.startButton)
+
+
+        startButton.setOnClickListener() {
+            val playern1: String = Player1.text.toString()
+            val playern2: String = Player2.text.toString()
+
+            if (playern1.isNotEmpty() && playern2.isNotEmpty()) {
+                val intent = Intent(this, Tictactoe::class.java)
+                intent.putExtra("player 1", "$playern1's turn")
+                intent.putExtra("player 2", "$playern2's turn")
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Please at first input player names",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kreswikinoliki/Tictactoe.kt
@@ -0,0 +1,230 @@
+package com.example.kreswikinoliki
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class Tictactoe : AppCompatActivity(), View.OnClickListener {
+    private lateinit var Button1: Button
+    private lateinit var Button2: Button
+    private lateinit var Button3: Button
+    private lateinit var Button4: Button
+    private lateinit var Button5: Button
+    private lateinit var Button6: Button
+    private lateinit var Button7: Button
+    private lateinit var Button8: Button
+    private lateinit var Button9: Button
+    private lateinit var resetButton: Button
+    private lateinit var gameText: TextView
+    private lateinit var score1: TextView
+    private lateinit var score2: TextView
+    private var activePlayer = 1
+    private var firstPlayer = ArrayList<Int>()
+    private var secPlayer = ArrayList<Int>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tictactoe)
+        init()
+
+
+    }
+
+    private fun init() {
+        Button1 = findViewById(R.id.Button1)
+        Button2 = findViewById(R.id.Button2)
+        Button3 = findViewById(R.id.Button3)
+        Button4 = findViewById(R.id.Button4)
+        Button5 = findViewById(R.id.Button5)
+        Button6 = findViewById(R.id.Button6)
+        Button7 = findViewById(R.id.Button7)
+        Button8 = findViewById(R.id.Button8)
+        Button9 = findViewById(R.id.Button9)
+        resetButton = findViewById(R.id.resetButton)
+        gameText = findViewById(R.id.gameText)
+        score1 = findViewById(R.id.score1)
+        score2 = findViewById(R.id.score2)
+        Button1.setOnClickListener(this)
+        Button2.setOnClickListener(this)
+        Button3.setOnClickListener(this)
+        Button4.setOnClickListener(this)
+        Button5.setOnClickListener(this)
+        Button6.setOnClickListener(this)
+        Button7.setOnClickListener(this)
+        Button8.setOnClickListener(this)
+        Button9.setOnClickListener(this)
+    }
+
+    override fun onClick(clickedView: View?) {
+        if (clickedView is Button) {
+            var buttonNumber = 0
+
+            when (clickedView.id) {
+                R.id.Button1 -> buttonNumber = 1
+                R.id.Button2 -> buttonNumber = 2
+                R.id.Button3 -> buttonNumber = 3
+                R.id.Button4 -> buttonNumber = 4
+                R.id.Button5 -> buttonNumber = 5
+                R.id.Button6 -> buttonNumber = 6
+                R.id.Button7 -> buttonNumber = 7
+                R.id.Button8 -> buttonNumber = 8
+                R.id.Button9 -> buttonNumber = 9
+
+
+            }
+            if (buttonNumber != 0) {
+                playGame(clickedView, buttonNumber)
+            }
+        }
+
+
+    }
+
+
+    private fun playGame(clickedView: Button, buttonNumber: Int) {
+
+        if (activePlayer == 1) {
+            clickedView.text = "X"
+            findViewById<TextView>(R.id.gameText).text = intent?.extras?.getString("player 2")
+            clickedView.setBackgroundColor(Color.BLUE)
+            firstPlayer.add(buttonNumber)
+            activePlayer = 2
+        } else {
+            clickedView.text = "O"
+            findViewById<TextView>(R.id.gameText).text = intent?.extras?.getString("player 1")
+            clickedView.setBackgroundColor(Color.RED)
+            secPlayer.add(buttonNumber)
+            activePlayer = 1
+        }
+        clickedView.isEnabled = false
+        check()
+
+    }
+
+    private fun check() {
+        var winner = 0
+        var sc1 = 0
+        var sc2 = 0
+
+        if (firstPlayer.contains(1) && firstPlayer.contains(2) && firstPlayer.contains(3)) {
+            winner = 1
+        }
+        if (secPlayer.contains(1) && secPlayer.contains(2) && secPlayer.contains(3)) {
+            winner = 2
+        }
+        if (firstPlayer.contains(4) && firstPlayer.contains(5) && firstPlayer.contains(6)) {
+            winner = 1
+        }
+        if (secPlayer.contains(4) && secPlayer.contains(5) && secPlayer.contains(6)) {
+            winner = 2
+        }
+        if (firstPlayer.contains(7) && firstPlayer.contains(8) && firstPlayer.contains(9)) {
+            winner = 1
+        }
+        if (secPlayer.contains(7) && secPlayer.contains(8) && secPlayer.contains(9)) {
+            winner = 2
+        }
+        if (firstPlayer.contains(1) && firstPlayer.contains(4) && firstPlayer.contains(7)) {
+            winner = 1
+        }
+        if (secPlayer.contains(1) && secPlayer.contains(4) && secPlayer.contains(7)) {
+            winner = 2
+        }
+        if (firstPlayer.contains(2) && firstPlayer.contains(5) && firstPlayer.contains(8)) {
+            winner = 1
+        }
+        if (secPlayer.contains(2) && secPlayer.contains(5) && secPlayer.contains(8)) {
+            winner = 2
+        }
+        if (firstPlayer.contains(3) && firstPlayer.contains(6) && firstPlayer.contains(9)) {
+            winner = 1
+        }
+        if (secPlayer.contains(3) && secPlayer.contains(6) && secPlayer.contains(9)) {
+            winner = 2
+        }
+        if (firstPlayer.contains(1) && firstPlayer.contains(5) && firstPlayer.contains(9)) {
+            winner = 1
+        }
+        if (secPlayer.contains(1) && secPlayer.contains(5) && secPlayer.contains(9)) {
+            winner = 2
+        }
+        if (firstPlayer.contains(3) && firstPlayer.contains(5) && firstPlayer.contains(7)) {
+            winner = 1
+        }
+        if (secPlayer.contains(3) && secPlayer.contains(5) && secPlayer.contains(7)) {
+            winner = 2
+        }
+        if ((firstPlayer.contains(1) or secPlayer.contains(1)) && (firstPlayer.contains(2) or secPlayer.contains(
+                2
+            )) && (firstPlayer.contains(3) or secPlayer.contains(3)) && (firstPlayer.contains(4) or secPlayer.contains(
+                4
+            )) && (firstPlayer.contains(5) or secPlayer.contains(5)) && (firstPlayer.contains(6) or secPlayer.contains(
+                6
+            )) && (firstPlayer.contains(7) or secPlayer.contains(7)) && (firstPlayer.contains(8) or secPlayer.contains(
+                8
+            )) && (firstPlayer.contains(9) or secPlayer.contains(9))
+        ) {
+            winner = 3
+        }
+        if (winner == 1) {
+            Toast.makeText(this, "X player won", Toast.LENGTH_SHORT).show()
+            sc1 += 1
+            score1.text = "O score: $sc1"
+            clean()
+
+        } else if (winner == 2) {
+            sc2 += 1
+            Toast.makeText(this, "O player won", Toast.LENGTH_SHORT).show()
+            score2.text = "O score: $sc2"
+            clean()
+        }
+        else if (winner == 3){
+            Toast.makeText(this, "It's tie", Toast.LENGTH_SHORT).show()
+            clean()
+        }
+        resetButton.setOnClickListener{
+            clean()
+        }
+
+    }
+
+    private fun clean() {
+        Button1.isEnabled = true
+        Button1.text = ""
+        Button1.setBackgroundColor(Color.GRAY)
+        Button2.isEnabled = true
+        Button2.text = ""
+        Button2.setBackgroundColor(Color.GRAY)
+        Button3.isEnabled = true
+        Button3.text = ""
+        Button3.setBackgroundColor(Color.GRAY)
+        Button4.isEnabled = true
+        Button4.text = ""
+        Button4.setBackgroundColor(Color.GRAY)
+        Button5.isEnabled = true
+        Button5.text = ""
+        Button5.setBackgroundColor(Color.GRAY)
+        Button6.isEnabled = true
+        Button6.text = ""
+        Button6.setBackgroundColor(Color.GRAY)
+        Button7.isEnabled = true
+        Button7.text = ""
+        Button7.setBackgroundColor(Color.GRAY)
+        Button8.isEnabled = true
+        Button8.text = ""
+        Button8.setBackgroundColor(Color.GRAY)
+        Button9.isEnabled = true
+        Button9.text = ""
+        Button9.setBackgroundColor(Color.GRAY)
+        firstPlayer.clear()
+        secPlayer.clear()
+
+    }
+
+
+}
+
